107936048e7828492684478d337c90ed3dce1456
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2023-10-09 21:52:52 +0530

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -7,9 +7,9 @@ def data_preprocess():
     data = data_analysis()
     data.drop(columns=["sku","lead_time"],axis=1,inplace=True)
     data = data.dropna(axis=0, how='any')
-    df.replace(to_replace = -99, value = np.nan)
-    df["perf_6_month_avg"] = df["perf_6_month_avg"].fillna(df["perf_6_month_avg"].median(), inplace=True)
-    df["perf_12_month_avg"] = df["perf_12_month_avg"].fillna(df["perf_12_month_avg"].median(), inplace=True)
+    data.replace(to_replace = -99, value = np.nan)
+    data["perf_6_month_avg"] = data["perf_6_month_avg"].fillna(data["perf_6_month_avg"].median(), inplace=True)
+    data["perf_12_month_avg"] = data["perf_12_month_avg"].fillna(data["perf_12_month_avg"].median(), inplace=True)
     print ("Missing values after removal of rows with empty values\n\n",data.isnull().any(),sep='')
     print(data)
     return data
