dcb1f31f4eb5d1c1ffb23923acdf3a0db6664153
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2023-10-09 16:38:17 +0530

    Create datavisualization.py

---
+++ datavisualization.py
@@ -0,0 +1,18 @@
+import seaborn as sns
+from data_preprocessing import data_preprocess
+import matplotlib.pyplot as plt
+
+
+def data_visualization():
+
+    data=data_preprocess()
+    names=["national_inv","sales_1_month", "sales_9_month", "min_bank", "perf_6_month_avg","perf_12_month_avg", "local_bo_qty"]
+
+    for i,j in enumerate(names):
+        plt.figure(figsize=(12,15))
+        plt.subplot(8,5,i+1)
+        sns.boxplot(y=j,data=data)
+
+    return data
+
+data_visualization()
