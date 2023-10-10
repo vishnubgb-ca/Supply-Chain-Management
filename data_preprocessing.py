8b0d44432b9ce88db29f8233fa68c67fc51275cd
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2023-10-10 17:38:33 +0530

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -13,15 +13,8 @@ def data_preprocess():
     data["perf_12_month_avg"] = data["perf_12_month_avg"].fillna(data["perf_12_month_avg"].median())
     print ("Missing values after removal of rows with empty values\n\n",data.isnull().any(),sep='')
     print(data)
-    le=LabelEncoder()
     
-    data['potential_issue']=le.fit_transform(data['potential_issue'])
-    data['deck_risk']=le.fit_transform(data['deck_risk'])
-    data['oe_constraint']=le.fit_transform(data['oe_constraint'])
-    data['ppap_risk']=le.fit_transform(data['ppap_risk'])
-    data['stop_auto_buy']=le.fit_transform(data['stop_auto_buy'])
-    data['rev_stop']=le.fit_transform(data['rev_stop'])
-    data['went_on_backorder']=le.fit_transform(data['went_on_backorder'])
+   
     #names=["national_inv",	"in_transit_qty",	"forecast_3_month",	"forecast_6_month"	,"forecast_9_month"	,"sales_1_month", "sales_3_month", "sales_6_month", "sales_9_month", "min_bank", "pieces_past_due", "perf_6_month_avg","perf_12_month_avg", "local_bo_qty"]
 
     #outlier detection
