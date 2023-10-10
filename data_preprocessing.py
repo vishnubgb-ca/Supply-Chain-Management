6f36ef84edfcad9049c4a045012fe05ed69c7efd
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2023-10-10 09:53:56 +0530

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -7,7 +7,7 @@ def data_preprocess():
     data = data_analysis()
     data.drop(columns=["sku","lead_time"],axis=1,inplace=True)
     data = data.dropna(axis=0, how='any')
-    data.replace(to_replace = -99, value = np.nan)
+    data = data.replace(to_replace = -99, value = np.nan)
     data["perf_6_month_avg"] = data["perf_6_month_avg"].fillna(data["perf_6_month_avg"].median())
     data["perf_12_month_avg"] = data["perf_12_month_avg"].fillna(data["perf_12_month_avg"].median())
     print ("Missing values after removal of rows with empty values\n\n",data.isnull().any(),sep='')
