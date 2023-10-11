331bb42dd529d020f77d2e37b7f5b7dbdfcd1093
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2023-10-11 14:48:40 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -16,7 +16,7 @@ def data_visualization():
         sns.displot(x=data[i])
         plt.xlabel(i)
         plt.title(i)
-    plt.show()
+        plt.show()
     
     names=["national_inv","sales_1_month", "sales_9_month", "min_bank", "perf_6_month_avg","perf_12_month_avg", "local_bo_qty"]
 
@@ -24,14 +24,14 @@ def data_visualization():
         plt.figure(figsize=(10,8))
         #plt.subplot(8,5,i+1)
         sns.boxplot(y=j,data=data)
-    plt.show()
+        plt.show()
 
     for i in names:
         plt.figure(figsize=(10,8))
         plt.bar(data["went_on_backorder"],data[i])
         plt.xlabel(i)
         plt.title(i)
-    plt.show()
+        plt.show()
 
     
     return data
