59ce326252eb07de359f14a7c3f4cfa04bd14ed6
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2023-10-11 14:25:28 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -13,11 +13,26 @@ def data_visualization():
     names=["national_inv","sales_1_month", "sales_9_month", "min_bank", "perf_6_month_avg","perf_12_month_avg", "local_bo_qty"]
 
     for i,j in enumerate(names):
-        plt.figure(figsize=(5,5))
+        plt.figure(figsize=(10,8))
         #plt.subplot(8,5,i+1)
         sns.boxplot(y=j,data=data)
         plt.show()
 
+    for i in names:
+        plt.figure(figsize=(10,8))
+        plt.bar(df["went_on_backorder"],df[i])
+        plt.xlabel(numeric)
+        plt.title(numeric)
+        plt.show()
+
+    names_cat=["deck_risk","ppap_risk","stop_auto_buy"]
+    for i in names_cat:
+        plt.figure(figsize=(10,8))
+        sns.displot(x=df[i])
+        plt.xlabel(i)
+        plt.title(i)
+        plt.show()
+    
     return data
 
 data_visualization()
