892f4fdb99c080e69588d8e36d5628f9c36c24cf
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2023-11-12 16:46:46 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -1,6 +1,7 @@
 from data_preprocessing import data_preprocess
 import pandas as pd
 import plotly.express as px
+from IPython.display import Image
 
 def data_visualization():
 
@@ -13,12 +14,16 @@ def data_visualization():
     for i in names_cat:
         list = []
         list.append(i)
-        df = data.groupby(by=list).size().reset_index(name="counts")
-        fig=px.bar(data_frame=df, x=i, y="counts",color=i)
+        data = df.groupby(by=list).size().reset_index(name="counts")
+        fig=px.bar(data_frame=data, x=i, y="counts",color=i)
         fig.update_layout(plot_bgcolor = "black")
         fig.update_xaxes(showgrid=False)
         fig.update_yaxes(showgrid=False)
+        #img_bytes = fig.to_image(format="png")
+        #f2 = go.FigureWidget(fig)
         fig.show()
+        img_bytes = fig.to_image(format="png")
+        Image(img_bytes)
     
     names=["national_inv","sales_1_month", "sales_9_month", "min_bank", "perf_6_month_avg","perf_12_month_avg", "local_bo_qty"]
 
