b1fffcf12170f37716f96f2fb600cc93012bf262
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2024-02-12 13:16:07 +0530

    Update loading_data.py

--- loading_data.py
+++ loading_data.py
@@ -8,8 +8,7 @@ def loading_data():
     username = os.environ.get("DB_USER_NAME")
     password = os.environ.get("DB_PASSWORD")
     databasename = os.environ.get("DB_DB_NAME")
-    #connection = pymysql.connect(host=hostname, user=username,password=password,database=databasename,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
-    connection = pymysql.connect(host='172.31.61.44', user='mlanglessyntacticai', password='mlanglessyntacticai123', database='syntactic_ai_pipelines_data')
+    connection = pymysql.connect(host=hostname, user=username,password=password,database=databasename,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
     return data
 
 loading_data()
