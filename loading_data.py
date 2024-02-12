99ac91fa7412344b6b1b1023f6a388d3b98f344c
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2024-02-12 13:23:53 +0530

    Update loading_data.py

--- loading_data.py
+++ loading_data.py
@@ -8,7 +8,7 @@ def loading_data():
     username = os.getenv("DB_USER_NAME")
     password = os.getenv("DB_PASSWORD")
     databasename = os.getenv("DB_DB_NAME")
-    connection = pymysql.connect(host=hostname, user=username,password=password,database=databasename,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+    connection = pymysql.connect(host='172.31.61.44', user=username,password=password,database=databasename,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
     sql_query = "SELECT * FROM backorder_prediction"
     data = pd.read_sql(sql_query, connection)
     connection.close()
