50962bddb858b59c9e144e75b9d74fade57c8db5
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2024-02-12 13:22:27 +0530

    Update loading_data.py

--- loading_data.py
+++ loading_data.py
@@ -4,10 +4,10 @@ import os
 
 def loading_data():
     #data = pd.read_csv('./Kaggle_Test_Dataset_v2.csv')
-    hostname = os.environ.get("DB_HOST_NAME")
-    username = os.environ.get("DB_USER_NAME")
-    password = os.environ.get("DB_PASSWORD")
-    databasename = os.environ.get("DB_DB_NAME")
+    hostname = os.getenv("DB_HOST_NAME")
+    username = os.getenv("DB_USER_NAME")
+    password = os.getenv("DB_PASSWORD")
+    databasename = os.getenv("DB_DB_NAME")
     connection = pymysql.connect(host=hostname, user=username,password=password,database=databasename,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
     sql_query = "SELECT * FROM backorder_prediction"
     data = pd.read_sql(sql_query, connection)
