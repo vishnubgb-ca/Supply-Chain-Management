7d0919c63de8a778d77efbceb48db4f16bcccb48
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2024-02-12 12:56:23 +0530

    Update loading_data.py

--- loading_data.py
+++ loading_data.py
@@ -1,7 +1,14 @@
 import  pandas as pd
+import pymysql
+import os
 
 def loading_data():
-    data = pd.read_csv('./Kaggle_Test_Dataset_v2.csv')
+    #data = pd.read_csv('./Kaggle_Test_Dataset_v2.csv')
+    hostname = os.environ.get("DB_HOST_NAME")
+    username = os.environ.get("DB_USER_NAME")
+    password = os.environ.get("DB_PASSWORD")
+    databasename = os.environ.get("DB_DB_NAME")
+    connection = pymysql.connect(host=hostname, user=username,password=password,database=databasename,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
     return data
 
 loading_data()
